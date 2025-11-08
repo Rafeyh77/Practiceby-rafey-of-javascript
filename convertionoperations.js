ea7b86096d9d75ad88fb17b9a3d2f837ef52598e
Rafey Hussain <hussainrafey7@gmail.com>
2025-11-08 12:37:23 +0500

    new datatype convertion

---
+++ convertionoperations.js
@@ -0,0 +1,40 @@
+let score = "22"; //string
+
+/*
+console.log(typeof score);
+
+let convertedScore = Number(score); //number
+console.log(typeof convertedScore);
+
+let result = true;
+let convertedResult = Number(result);
+console.log(convertedResult); //1
+
+let result1 = 1;
+let convertedResult1 = Boolean(result1);
+console.log(convertedResult1); //true
+
+
+let result2 = 0;
+let convertedResult2 = Boolean(result2);
+console.log(convertedResult2); //false
+
+
+let result3 = "";
+let convertedResult3 = Boolean(result3);
+console.log(convertedResult3); //false
+
+
+let result4 = "Rafey";
+let convertedResult4 = Boolean(result4);
+console.log(convertedResult4); //true
+
+let result5 = "1234ab";
+let convertedResult5 = Number(result5);
+console.log(convertedResult5); //Nan
+*/
+
+let result6 = "1234ab";
+let convertedResult6 = String(result6);
+console.log(convertedResult6); //string
+console.log(typeof convertedResult6);
