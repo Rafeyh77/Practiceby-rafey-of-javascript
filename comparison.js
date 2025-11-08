5facecce463d4a771e95fb4ce0e554099f35650f
Rafey Hussain <hussainrafey7@gmail.com>
2025-11-08 13:05:49 +0500

    convertion added

---
+++ comparison.js
@@ -0,0 +1,25 @@
+/*
+console.log( 2> 1);
+console.log(2 < 1);
+console.log(2 >= 2);
+console.log(2 <= 2);
+console.log(2 == 2);
+console.log(2 != 1);
+
+
+//when ever you compare two different types javascript converts one type to another type
+// when ever you compare two values in js their datatype must be same 
+
+console.log(2 == "2");//true because string converted to number
+
+
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0) // null coverted to 0
+
+*/
+
+// === strict equality operator
+console.log(2 === "2")//false
+console.log(2 !== "2")//true
---
+++ operations.js
@@ -0,0 +1,29 @@
+let val = 4
+let negvalue = -val
+/*
+console.log("Value is: " + val);
+
+
+let str1 = "hello"
+let str2 = "Rafey"
+
+let str3 = str1 + " " + str2
+console.log("Concatenated String: " + str3);
+
+
+
+
+console.log("2" + 1); // if string comes first then it will convert result to string 
+console.log("2" + 1 + 1);
+console.log(2 + 2 + "1");// if number comes first will convert to numeric
+
+console.log(2 + 2 - 1 % 3); //not appreciatedd in industire messy code
+
+console.log(2 + 2 + "1aaa");
+
+*/
+
+let gamecount = 10;
+gamecount++;//postfix
+++gamecount;//prefix
+console.log(gamecount)
